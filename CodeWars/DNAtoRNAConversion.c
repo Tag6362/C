68203afd96ed9a3ba351a0e7928315f63cc3186c
Tag6362 <46137705+Tag6362@users.noreply.github.com>
2022-07-02 11:38:32 +0200

    Update DNAtoRNAConversion.c

--- CodeWars/DNAtoRNAConversion.c
+++ CodeWars/DNAtoRNAConversion.c
@@ -1,1 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
 
+char *dna_to_rna(const char *dna)
+{
+  int size = strlen(dna);
+  char *rna = malloc(size*sizeof(char)+1);
+
+  for (int i = 0; i < (size+1); i++) {
+    if (dna[i] == 'T') {
+      rna[i] = 'U';
+    } else {
+      rna[i] = dna[i];
+    }
+  }
+  return rna;
+}

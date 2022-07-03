738986b5063bfc2e1f203de18eadc0c73cfe1540
Tom <tag6362@gmail.com>
2022-07-03 10:06:45 +0200

    QuickPublish from Tag6362

--- test.c
+++ test.c
@@ -1,8 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main() {
-	puts("Hello world!");
+int main(int argc, char *argv[]) {
+	char entry[20];
+	
+	puts("Enter a string");
+	fgets(entry, 20, stdin);
+
+	int len = 0;
+	int i = 0;
+
+	while (entry[i] != '\0')  {
+		len++; i++;
+	}
+	printf("Length of the string is : %d, or %d without the \\n", len, (len-1));
 	return 0;
 }
 
